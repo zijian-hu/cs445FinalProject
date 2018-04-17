8080785dcc7797fb32ac1b603ef9d97a93eb519e
jluceno <luceno@usc.edu>
2018-04-17 15:52:28 -0700

    Cleaned up debug code. Added: odometry updating, odometry angle clamping, update functions, drive function. Modified some parameters.

--- complementary_filter.py
+++ complementary_filter.py
@@ -1,19 +1,43 @@
 # https://codeshare.io/5wlweB
 import numpy as np
-
+import math
 
 class ComplementaryFilter:
-    def __init__(self, odometry, tracker, alpha):
+    def __init__(self, odometry, tracker, time, update_interval, alpha):
         self.alpha = np.array(alpha)
         self.odometry = odometry
         self.tracker = tracker
+        self.update_interval = update_interval
+        self.time = time
+
+        # variables
+        self.time_last_update = self.time.time()
 
         self.x = 0.0
         self.y = 0.0
         self.theta = 0.0
 
     def update(self):
-        odometry_list = (self.odometry.x, self.odometry.y, self.odometry.theta)
+        # convert odometry angles to fit in the range -pi to pi
+        odometry_converted = self.odometry.theta
+        if self.odometry.theta > math.pi:
+            odometry_converted = self.odometry.theta - 2*math.pi
+        elif self.odometry.theta < -math.pi:
+            odometry_converted = self.odometry.theta + 2*math.pi
+
+        odometry_list = (self.odometry.x, self.odometry.y, odometry_converted)
         tracker_list = (self.tracker.x, self.tracker.y, self.tracker.theta)
 
+        # update odometry in set intervals
+        # TODO add high pass filtering
+        if self.time.time() - self.time_last_update > self.update_interval:
+            self.update_odometry()
+            self.time_last_update = self.time.time()
+
         self.x, self.y, self.theta = self.alpha * tracker_list + (1 - self.alpha) * odometry_list
+
+    def update_odometry(self):
+        print('=== Odometry updated ===')
+        self.odometry.x = self.tracker.x
+        self.odometry.y = self.tracker.y
+        self.odometry.theta = self.tracker.theta
